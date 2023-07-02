79cb8a98e684a4d01c06590fe2656d96d14bd9c2
CoBre03 <137793665+CoBre03@users.noreply.github.com>
2023-07-02 15:22:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import matplotlib.pyplot as plt # for plotting
-import numpy as np # for transformation
+import matplotlib.pyplot as plt
+import numpy as np
 
-import torch # PyTorch package
-import torchvision # load datasets
+import torch                                # PyTorch package
+import torchvision                          # load datasets
 import torchvision.transforms as transforms # transform data
-import torch.nn as nn # basic building block for neural neteorks
-import torch.nn.functional as F # import convolution functions like Relu
-import torch.optim as optim # optimzer
+import torch.nn as nn                       # basic building block for neural neteorks
+import torch.nn.functional as F             # import convolution functions like Relu
+import torch.optim as optim                 # optimzer
 
 import torch.utils.data
 
-if __name__ == '__main__':
+TRAIN_DATA_PATH = "./data/train/"
+TEST_DATA_PATH = "./data/test/"
 
+def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
     class Net(nn.Module):
         ''' Models a simple Convolutional Neural Network'''
@@ -26,27 +27,30 @@ if __name__ == '__main__':
             self.conv1 = nn.Conv2d(3, 6, 5)
             # Max pooling over a (2, 2) window
             self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5x5 from image dimension
+            self.conv2 = nn.Conv2d(6, 8, 5)
+            self.fc1 = nn.Linear(8 * 5 * 5, 120)  # 5x5 from image dimension
             self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
+            self.fc3 = nn.Linear(84, 1)
+            # self.gfy1 = nn.Linear(120, fc3[1]*num_batches/100)
 
         def forward(self, x):
             ''' the forward propagation algorithm '''
             x = self.pool(F.relu(self.conv1(x)))
             x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, 16 * 5 * 5)
+            x = x.view(-1, 8 * 5 * 5)
             x = F.relu(self.fc1(x))
             x = F.relu(self.fc2(x))
             x = self.fc3(x)
             return x
 
-
     net = Net().to(device)
 
-    transform = transforms.Compose( # composing several transforms together
-        [transforms.ToTensor(), # to tensor object
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) # mean = 0.5, std = 0.5
+    scaler = 0.8
+    transform = transforms.Compose([
+        transforms.Resize((int(1328 * scaler), int(1160 * scaler))),  # from 1328x1160
+        transforms.ToTensor(),  # to tensor object
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # mean = 0.5, std = 0.5
+    ])
 
     # set batch_size
     batch_size = 4
@@ -55,36 +59,34 @@ if __name__ == '__main__':
     num_workers = 2
 
     # load train data
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
+    trainset = torchvision.datasets.ImageFolder(root=TRAIN_DATA_PATH, transform=transform)
+    ## append also transformed data to artificially generate more input
+    ## TODO: search for how to append
+    # trainset.append( torchvision.datasets.ImageFolder(root=TRAIN_DATA_PATH,
+    #                               transform=transform,
+    #                               target_transform=transforms.GaussianBlur) )
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=num_workers)
 
     # load test data
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
+    testset = torchvision.datasets.ImageFolder(root=TEST_DATA_PATH, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=num_workers)
 
-    # put 10 classes into a set
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
+    # put 1 class into a set
+    classes = 'pneumonia'
 
     def imshow(img):
-      ''' function to show image '''
-      img = img / 2 + 0.5 # unnormalize
-      npimg = img.numpy() # convert to numpy objects
+        ''' function to show image '''
+        img = img / 2 + 0.5  # unnormalize
+        npimg = img.numpy()  # convert to numpy objects
 
-
-      plt.imshow(np.transpose(npimg, (1, 2, 0)))
-      plt.show()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
 
     # get random training images with iter function
     dataiter = iter(trainloader)
     images, labels = next(dataiter)
-
 
     imshow(torchvision.utils.make_grid(images))
 
@@ -93,8 +95,6 @@ if __name__ == '__main__':
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
 
     for epoch in range(1):  # loop over the dataset multiple times
 
@@ -113,12 +113,11 @@ if __name__ == '__main__':
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item() #loss of entire mini-batch
+            running_loss += loss.item()  # loss of entire mini-batch
             if i % 2000 == 1999:
                 print('[%d, %5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
-
 
     # save
     PATH = './cifar_net.pth'
@@ -137,19 +136,22 @@ if __name__ == '__main__':
     correct = 0
     total = 0
 
-
     with torch.no_grad():
         for data in testloader:
             images, labels = data
             outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
 
-            print (labels.size(0))
-
+            print(labels.size(0))
 
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
     print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+
+if __name__ == '__main__':
+    main()
+
 
 
